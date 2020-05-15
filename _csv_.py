809d61d30f1ad580ae48ac10350b2587c3e67462
Sherlock Wisdom <wisdomnji@gmail.com>
2020-05-15 20:08:17 +0100

    it works

--- _csv_.py
+++ _csv_.py
@@ -1,5 +1,14 @@
 #!/bin/python
 
+# How I got things working
+# pip3 install pandas
 
 from pandas import read_csv
 import pandas as pd
+
+
+file = 'sample_data.csv'
+df = pd.read_csv( file )
+
+print("Type: ", type(df))
+print( df )
