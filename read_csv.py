a649b77421b1a41bf2b77fe9f915ed233e64936a
Sherlock Wisdom <wisdomnji@gmail.com>
2020-06-03 13:05:08 +0100

    protptype working

--- read_csv.py
+++ read_csv.py
@@ -10,24 +10,29 @@ file1 = 'sample_data.csv'
 file2 = 'sample_data2.csv'
 file3 = 'sample_data3.csv'
 
-elements_file1 = pd.read_csv( file1 )['name']
-elements_file2 = pd.read_csv( file2 )['name']
-elements_file3 = pd.read_csv( file3 )['name']
+elements_file1 = pd.read_csv( file1 )['name'].tolist()
+elements_file2 = pd.read_csv( file2 )['name'].tolist()
+elements_file3 = pd.read_csv( file3 )['name'].tolist()
 
-tetha = elements_file1 + elements_file2 + elements_file3
+# print (type(elements_file1))
+
+tetha = list(set(elements_file1 + elements_file2 + elements_file3))
+print( tetha )
 
 n_tetha = len(tetha)
-F = [file1, file2, file3]
-y[len(F)][n_tetha]
+F = [elements_file1, elements_file2, elements_file3]
+list_file = [file1, file2, file3]
+# y[len(F)][n_tetha]
 
-for i in tetha:
-    for j in F:
-        for k in F[j]:
+y = []
+
+for i in range(len(tetha)):
+    for j in range(len(F)):
+        for k in range(len(F[j])):
             if F[j][k] == tetha[i] :
-                y[j][i] = 1
+                y.append([list_file[j],tetha[i],1])
                 break
 
-        y[j][i] = 0
+        y.append([list_file[j],tetha[i],0])
 
 print( y )
-
