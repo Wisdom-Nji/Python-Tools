b22448d89e41c2c9dfe3835393a46eed741786e6
Sherlock Wisdom <wisdomnji@gmail.com>
2020-06-13 15:11:49 +0100

    updated, code with Mariette to create Boolean table

--- read_csv.py
+++ read_csv.py
@@ -22,7 +22,6 @@ tetha = list(set(elements_file1 + elements_file2 + elements_file3))
 n_tetha = len(tetha)
 F = [elements_file1, elements_file2, elements_file3]
 list_file = [file1, file2, file3]
-# y[len(F)][n_tetha]
 
 y = []
 
@@ -32,7 +31,9 @@ for i in range(len(tetha)):
         for k in range(len(F[j])):
             if F[j][k] == tetha[i] :
                 # y.append([list_file[j],tetha[i],1])
-                y.append([list_file[j], tetha[i], 1])
+                mariette = [list_file[j], tetha[i], 1]
+                # array = [ filename, element, bool ]
+                y.append( mariette )
                 found = True
                 break
 
@@ -41,6 +42,32 @@ for i in range(len(tetha)):
             y.append([list_file[j], tetha[i], 0])
 
 
-for items in y:
-    print(items)
+# Declaring empty arrays for files and elements so to have them unique
+list_of_file_from_y = []
+list_of_elements_from_y = []
+
+# looping through y to get the names of files and the individual elements, then storing the output in their respective arrays
+for item in y:
+    list_of_file_from_y.append( item[0] ) # for each item in y, we take out the filename
+    list_of_elements_from_y.append( item[1] ) # for each item in y, we take out the element
+
+list_of_file_from_y = list(set(list_of_file_from_y)) # we make the filenames unique thus no repetition
+list_of_file_from_y.sort() # we sort the filenames in ascending order
+
+list_of_elements_from_y = list(set(list_of_elements_from_y)) # we repeat the above steps for the elements here
+# list_of_elements_from_y.sort()
+
+print( "Element", end=' ') # we print element in the first column of our table, we end with ' ' because we want to continue on the same line to print the filenames
+
+for i in range(len(list_of_file_from_y)): # we loop through the filenames and print them one at a time, ending with ' ' for the reasons explained above
+    print( list_of_file_from_y[i], end=' ')
+
+print() # this takes us to the next row
+
+for element in list_of_elements_from_y: # we loop through every element and print them for each row, we end with ' ' so that we can continue on the same line
+    print( element, end= ' ')
+    for item in y: # we loop through y, to get the boolean of each matching file for each element
+        if item[1] == element : # we compare the element in each array of y to match the array of the row we currently are looping in
+            print( item[2], end= ' ') # print the booleans in single line
+    print()
 
